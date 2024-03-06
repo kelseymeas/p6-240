bdbd666689743ff3d9643a180be64819d2d3f5cc
kelseymeas <kelsey.meas@csu.fullerton.edu>
2024-03-06 02:16:23 -0800

    Added 'selectAndExecutableFunction()'

--- p6-240/p6-240.cpp
+++ p6-240/p6-240.cpp
@@ -4,12 +4,16 @@
 
 // This is pretty darn cool
 
+#include "p5-helpers.h"
 #include "p6-helpers.h"
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <cctype>
 #include <cstdlib>
+
+// Display Project Portfolio
+void selectAndExecuteFunction();
 
 // Project 6  -- Float Stack Manipulation
 void asm_Area_Perimeter();        // Q1 -- Calc Area/Perim 
@@ -19,10 +23,11 @@ void asm_Convenient_Store();      // Q3 -- Calc Receipt
 
 
 int main() { 
-    asm_Area_Perimeter();
+    //asm_Area_Perimeter();
     //asm_Quadratic_Formula_Add();
     //asm_Quadratic_Formula_Sub();
     //asm_Convenient_Store();
+    selectAndExecuteFunction();
 
     return 0;
 }
