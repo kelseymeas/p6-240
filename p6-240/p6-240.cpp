9bedddcc0087c705b3737bd4e80df0cf389da8b4
kelseymeas <kelsey.meas@csu.fullerton.edu>
2024-03-07 18:49:27 -0800

    added wrapper function to output projects

--- p6-240/p6-240.cpp
+++ p6-240/p6-240.cpp
@@ -2,33 +2,20 @@
 //// Kelsey Meas -- Project 6 -- CPSC 240-13 ////
 ////-----------------------------------------////
 
-// This is pretty darn cool
-
+// Project Headers
+#include "pWrapper-240.h"
+//#include "p1-helpers.h"
+//#include "p2-helpers.h"
+//#include "p3-helpers.h"
+//#include "p4-helpers.h"
 #include "p5-helpers.h"
 #include "p6-helpers.h"
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <cctype>
-#include <cstdlib>
 
 // Display Project Portfolio
 void selectAndExecuteFunction();
 
-// Project 6  -- Float Stack Manipulation
-void asm_Area_Perimeter();        // Q1 -- Calc Area/Perim 
-void asm_Quadratic_Formula_Add(); // Q2 -- Quadratic Formula +
-void asm_Quadratic_Formula_Sub(); // Q2 -- Quadratic Formula -
-void asm_Convenient_Store();      // Q3 -- Calc Receipt
-
-
 int main() { 
-    //asm_Area_Perimeter();
-    //asm_Quadratic_Formula_Add();
-    //asm_Quadratic_Formula_Sub();
-    //asm_Convenient_Store();
     selectAndExecuteFunction();
-
     return 0;
 }
 
